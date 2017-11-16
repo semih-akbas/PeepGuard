bbeb1c72bd7ab9fc0f9c947d68d366b697c196ff
Semih Akbas <semih.akbas@siemens.com>
2017-11-16 16:57:29 +0300

    todays changes

--- main.js
+++ main.js
@@ -1,75 +1,37 @@
 var express     = require('express');
 var app         = express();
 var http        = require('http').Server(app);
-var io          = require('socket.io')(http);
-var fs          = require('fs');
 var path        = require('path');
-var spawn       = require('child_process').spawn;
-var hbs         = require('express-handlebars');
+var exphbs         = require('express-handlebars');
 
 //TODO:Add GPIO package for Raspberry PI
 //var gpio = require('rpi-gpio');
 
-//app.set('views', (__dirname + '/../views'));
-app.engine('handlebars', hbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-//Routes
+//Routers
 var diagnosticsRouter = require('./routes/diagnostics');
 
-var proc;
+var hbs = exphbs.create({
+	defaultLayout: "home",
+	extname: "html",
+	helpers: {
+		section: function (name, options) {
+			if (!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
+});
+
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+
 app.use(function timeLog (req, res, next) {
     console.log('Request time: ', Date.now());
     next();
 });
 
-
-/*
-app.use('/', express.static(path.join(__dirname, 'stream')));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-*/
 app.use('/diagnostics', diagnosticsRouter);
 
-
-var sockets = {};
-io.on('connection', function (socket) {
-    sockets[socket.id] = socket;
-    console.log("Total clients connected : ", Object.keys(sockets).length);
-    socket.on('disconnect', function () {
-        delete sockets[socket.id];
-        // no more sockets, kill the stream
-        if (Object.keys(sockets).length == 0) {
-            app.set('watchingFile', false);
-            if (proc) proc.kill();
-            fs.unwatchFile('./stream/image_stream.jpg');
-        }
-    });
-    socket.on('start-stream', function () {
-        startStreaming(io);
-    });
-});
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
-function stopStreaming() {
-    if (Object.keys(sockets).length == 0) {
-        app.set('watchingFile', false);
-        if (proc) proc.kill();
-        fs.unwatchFile('./stream/image_stream.jpg');
-    }
-}
-function startStreaming(io) {
-    if (app.get('watchingFile')) {
-        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-        return;
-    }
-    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-    proc = spawn('raspistill', args);
-    console.log('Watching for changes...');
-    app.set('watchingFile', true);
-    fs.watchFile('./stream/image_stream.jpg', function (current, previous) {
-        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    })
-}
--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -4,37 +4,29 @@ var http        =   require('http').Server(express);
 var io          =   require("socket.io")(http);
 var fs          =   require("fs");
 var isPi        =   require('detect-rpi');
-var hbs         =   require('express-handlebars');
 
-//var chartKick   =   require("chartkick");
-var app = express();
-var router = express.Router();
-var currentTemperature = 0.0;
-
-router.get('/', function(req, res) {
-    console.log("HERE!!! " + __dirname);
+var router      =   express.Router();
+router.get('/', function(req, res) {   
     
-    setTimeout(UpdateTemperature, 10000, function(currentTemperature){});
-    //res.sendFile(path.resolve('pages/diagnostics.html'));
-    //res.render(path.resolve('pages/diagnostics.html'))
-    res.render('home', {deneme: 'hebele'});
+    //setTimeout(UpdateTemperature, 2000, function(nil){});
+    res.render('main', {deneme: 'hebele', points: '0,80 20,60 40,80 60,20'});
     
 });
 
 io.on('connection',function(socket){  
     console.log("A user is connected");
-    socket.on('status added',function(temperature){
+    /*socket.on('refresh-temperature',function(temperature){
         UpdateTemperature(function(currentTemperature){
         if(currentTemperature){
             io.emit('refresh feed',currentTemperature);
         } else {
             io.emit('error');
         }
-      });
-    });
+        });
+    });*/
 });
 
-var UpdateTemperature = function (callback) {
+var UpdateTemperature = function () {
     if(!isPi()){
         temperature = Math.random() * 100;
     }
@@ -44,10 +36,9 @@ var UpdateTemperature = function (callback) {
     }
 
     console.log("Current CPU Temperature: " + temperature + " °C");
-    callback(temperature);    
-    setTimeout(UpdateTemperature, 10000, function(temperature){});           
+    //setTimeout(UpdateTemperature, 2000, function(temperature){});           
 
-    io.emit('refresh feed',temperature);
+    io.emit('refresh-temperature',temperature);
 
     return;
 }
