f9d94420536f37a95e09c9ad473804f1caf3e8bb
Semih Akbas <semih.akbas@siemens.com>
2017-11-23 12:51:29 +0300

    added allowed origins to the requests updated v1

--- main.js
+++ main.js
@@ -40,9 +40,13 @@ app.use(function timeLog (req, res, next) {
 	console.log('Request time: ', Date.now());
 	var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://sammyhg.ddns.net:3000'];
 	var origin = req.headers.origin;
+	console.log(origin);
 	if(allowedOrigins.indexOf(origin) > -1){
 		 res.setHeader('Access-Control-Allow-Origin', origin);
 	}
+	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use('/diagnostics', diagnosticsRouter);
